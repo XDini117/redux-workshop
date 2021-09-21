01d7f61148a935eecca22ba0e9e6c6362b40139c
Daniel RL <dramonl@pagatodo.com>
2021-09-21 11:29:46 -0500

    Root version established

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-
-import storeFn from './redux/store';
-import App from './App';
-
-const store = storeFn();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <div>
+        <p>Redux - Workshop</p>
+    </div>,
     document.getElementById('root')
 );
