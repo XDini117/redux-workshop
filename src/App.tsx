886175507af125bdded607abb4ae97c867af4395
Daniel RL <dramonl@pagatodo.com>
2021-09-21 17:19:51 -0500

    Notes example added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import React from 'react';
+import { NewNoteInput } from './NewNoteInput';
+import { useSelector, useDispatch } from 'react-redux';
+import { NotesState } from './notesReducer';
+import { addNote } from './actions';
+
 function App() {
-    return <div>Redux - Workshop</div>;
+    const notes = useSelector<NotesState, NotesState['notes']>(state => state.notes);
+    const dispatch = useDispatch();
+
+    const onAddNote = (note: string) => {
+        dispatch(addNote(note));
+    };
+
+    return (
+        <>
+            <NewNoteInput addNote={onAddNote} />
+            <hr />
+            <ul>
+                {notes.map(note => {
+                    return <li key={note}>{note}</li>;
+                })}
+            </ul>
+        </>
+    );
 }
 
 export default App;
