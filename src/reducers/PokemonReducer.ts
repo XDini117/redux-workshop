322ab04d84896172b3ae1eade71deb30711a0461
Daniel <danielramon117@gmail.com>
2021-09-21 22:30:37 -0500

    PokemonReducer using actions

--- src/reducers/PokemonReducer.ts
+++ src/reducers/PokemonReducer.ts
@@ -1,9 +1,38 @@
-interface DefaultStateI {}
+import {
+    PokemonDispatchTypes,
+    PokemonType,
+    POKEMON_LOADING,
+    POKEMON_FAIL,
+    POKEMON_SUCCESS,
+} from '../actions/PokemonActionType';
 
-const defaultState: DefaultStateI = {};
+interface DefaultStateI {
+    loading: boolean;
+    pokemon?: PokemonType;
+}
 
-const pokemonReducer = (state: DefaultStateI = defaultState, action: any): DefaultStateI => {
-    return state;
+const defaultState: DefaultStateI = {
+    loading: false,
+};
+
+const pokemonReducer = (state: DefaultStateI = defaultState, action: PokemonDispatchTypes): DefaultStateI => {
+    switch (action.type) {
+        case POKEMON_FAIL:
+            return {
+                loading: false,
+            };
+        case POKEMON_LOADING:
+            return {
+                loading: true,
+            };
+        case POKEMON_SUCCESS:
+            return {
+                loading: false,
+                pokemon: action.payload,
+            };
+        default:
+            return state;
+    }
 };
 
 export default pokemonReducer;
