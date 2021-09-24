345054e1dd01acb9678c2aa1a98fbfab710d97ed
Daniel RL <dramonl@pagatodo.com>
2021-09-24 14:42:23 -0500

    actions creators created

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,30 @@
+import { Dispatch } from 'redux';
+
+import { Action } from '../actions';
+import { ActionType } from '../action-types';
+
+export const depositMoney = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.DEPOSIT,
+            payload: amount,
+        });
+    };
+};
+
+export const withdrawMoney = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.WITHDRAW,
+            payload: amount,
+        });
+    };
+};
+
+export const bankrupt = () => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.BANKRUPT,
+        });
+    };
+};
