ce21bb360e6e10676366e3b51ce430a0d2a404a8
Daniel RL <dramonl@pagatodo.com>
2021-09-29 16:59:20 -0500

    getAllUsers function adjusted

--- src/redux/reducers/users/users.thunks.ts
+++ src/redux/reducers/users/users.thunks.ts
@@ -4,15 +4,12 @@ import { getAllUsers } from '../../../services/users.service';
 import { Action, usersLoadError, usersLoadSuccess } from './users.actions';
 import { actionTypes } from './users.actionTypes';
 
-export const loadUsersAsync = () => async (dispatch: Dispatch<Action>) => {
+export const loadUsersAsync = () => (dispatch: Dispatch<Action>) => {
     dispatch({
         type: actionTypes.USERS_LOAD_START,
     });
 
-    try {
-        const response = await getAllUsers();
-        return dispatch(usersLoadSuccess(response.data));
-    } catch (error: any) {
-        return dispatch(usersLoadError(error.message));
-    }
+    return getAllUsers()
+        .then(data => dispatch(usersLoadSuccess(data)))
+        .catch(error => dispatch(usersLoadError(error.message)));
 };
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,3 +1,13 @@
 import apiClient from '../helper/apiClient';
 
-export const getAllUsers = () => apiClient.get('users');
+type UsersData = {
+    data: [
+        {
+            id: number;
+            name: string;
+            username: string;
+        }
+    ];
+};
+
+export const getAllUsers = () => apiClient.get('users').then(({ data }): UsersData => data);
